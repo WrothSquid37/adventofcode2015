f379018c3346e34728b17d8a010d52b1b665fab8
WrothSquid37 <cadeynrauscher@gmail.com>
2025-11-18 17:05:55 -0800

    Create next advent code projects files and initialize cargo

---
+++ day4/part1/advent4.rs
@@ -0,0 +1,11 @@
+extern crate md5;
+
+use md5::{Md5, Digest};
+
+fn main() {
+
+    println!("asdfasdfasdfasdfasdfasdfasdf");
+
+
+
+}
---
+++ day4/part1/target/debug/deps/part1-bcea3f8435d64750.d
@@ -0,0 +1,5 @@
+/home/cadey/rustprojects/adventofcode2015/day4/part1/target/debug/deps/part1-bcea3f8435d64750.d: src/main.rs
+
+/home/cadey/rustprojects/adventofcode2015/day4/part1/target/debug/deps/libpart1-bcea3f8435d64750.rmeta: src/main.rs
+
+src/main.rs:
