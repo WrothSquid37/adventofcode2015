46452f9f13ec95be4eb029e2a67732c994124d6b
WrothSquid37 <cadeynrauscher@gmail.com>
2025-11-18 17:31:28 -0800

    Fixed neccesary import and things to allow md5

--- day4/part1/src/main.rs
+++ day4/part1/src/main.rs
@@ -1,3 +1,6 @@
+
+
 fn main() {
-    println!("Hello, world!");
+    let digest = md5::compute("yzbqklnj");
+    println!("{:x}", digest);
 }
---
+++ day4/part1/target/debug/deps/part1-12fd37ab6a9d476a.d
@@ -0,0 +1,5 @@
+/home/cadey/rustprojects/adventofcode2015/day4/part1/target/debug/deps/part1-12fd37ab6a9d476a.d: src/main.rs
+
+/home/cadey/rustprojects/adventofcode2015/day4/part1/target/debug/deps/part1-12fd37ab6a9d476a: src/main.rs
+
+src/main.rs:
