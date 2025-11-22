af1b780d67f8c892578f5c98320ebd00ecd10bf8
WrothSquid37 <cadeynrauscher@gmail.com>
2025-11-22 15:36:33 -0800

    I forgot the changes. Just random stuff and i forgot to git add

---
+++ day6/target/debug/deps/day6-1f213027f5c80256.d
@@ -0,0 +1,5 @@
+/home/cadey/rustprojects/adventofcode2015/day6/target/debug/deps/day6-1f213027f5c80256.d: src/main.rs
+
+/home/cadey/rustprojects/adventofcode2015/day6/target/debug/deps/libday6-1f213027f5c80256.rmeta: src/main.rs
+
+src/main.rs:
---
+++ day6/target/debug/deps/day6-927936404baae72b.d
@@ -0,0 +1,5 @@
+/home/cadey/rustprojects/adventofcode2015/day6/target/debug/deps/day6-927936404baae72b.d: src/main.rs
+
+/home/cadey/rustprojects/adventofcode2015/day6/target/debug/deps/libday6-927936404baae72b.rmeta: src/main.rs
+
+src/main.rs:
