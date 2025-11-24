6de0e095c963b53bf44ce5cd8fc8a056553a73c5
WrothSquid37 <cadeynrauscher@gmail.com>
2025-11-23 20:51:37 -0800

    Continued progress on program

--- day6/src/main.rs
+++ day6/src/main.rs
@@ -2,12 +2,15 @@ use array2d::{Array2D, Error};
 use std::fs::File;
 use std::io::prelude::*;
 use std::path::Path;
+use std::io::{self, Write};
+
 
 fn main() {
     
     let mut grid: Array2D<i32> =  Array2D::filled_with(100, 1000, 1000);
-    
+    let mut edit_state: i8 = 0;
     let inputstring : String;
+    let mut first_set_found: bool = true;
 
     // Create a path to the desired file
     let path = Path::new("input.txt");
@@ -29,19 +32,51 @@ fn main() {
 
     let lines_vector: Vec<String> = inputstring.lines().map(String::from).collect();
 
-    // Look for tokens showing instructions
+    // Iterate through the array to find the lines needed
     for it in &lines_vector {
-        if check_for_tokens(it, "turn off") {
-            println!("Found turn off");
+
+        let mut first_found_cords: (i32, i32);
+        let mut second_found_cords: (i32, i32);
+
+        let cleaned_up_line = it.replacen("turn", "", 1);       
+
+        // Choose the path according to the "on" or "off" token. Flag the path
+        if cleaned_up_line.contains("on") { edit_state = 1 }
+        else if cleaned_up_line.contains("off") { edit_state = 2 }
+        else if cleaned_up_line.contains("toggle") { edit_state = 3 }
+
+        // Find the first set of cordinates and store them in seperate x and y
+        let split_words = cleaned_up_line.split(' ');
+        
+        for split_one in split_words {
+            
+            if split_one.contains(",") {
+                
+                let temp_split: Vec<&str> = split_one.split(',').collect();
+
+                println!("X: {} Y: {}", temp_split[0], temp_split[1]);
+
+                if !first_set_found {
+                    first_found_cords = (temp_split[0].parse().unwrap(), temp_split[1].parse().unwrap());
+                    first_set_found = true;
+                }
+                else {
+                    second_found_cords = (temp_split[0].parse().unwrap(), temp_split[1].parse().unwrap());
+                }
+
+                
+                
+            }
+
+            let mut buffer = String::new();
+            io::stdin().read_line(&mut buffer).unwrap();
+
         }
+
     }
 
-    println!("{:?}",lines_vector)
+    
 
 }
 
-fn check_for_tokens(search_string: String, test_string: String) -> bool {
 
-    return search_string.contains(&test_string)
-        
-}
